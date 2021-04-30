2cc233bda0056eb5b69c7eeee1d68d69e36a2fff
Stefan Baebler <stefan.baebler@gmail.com>
2021-04-30 20:00:59 +0200

    Add Janssen manufacturer

--- cepimose/parser.py
+++ cepimose/parser.py
@@ -135,13 +135,13 @@ def _parse_vaccines_supplied_by_manufacturer(
     ]
     parsed_data = []
 
-    if len(manufacturers) > 3:
+    if len(manufacturers) > 4:
         print(manufacturers)
         raise Exception("New manufacturer!")
 
     def get_manufacturer(num):
-        manu_keys = ["pfizer", "moderna", "az"]
-        if num > 2 or num == None:
+        manu_keys = ["pfizer", "moderna", "az", "janssen"]
+        if num > 3 or num == None:
             print(num)
             raise Exception("Missing manufacturer!")
         return manu_keys[num]
@@ -161,7 +161,7 @@ def _parse_vaccines_supplied_by_manufacturer(
             raise Exception("Unknown R value!")
 
         manu_row = VaccinationByManufacturerRow(
-            date=None, pfizer=None, moderna=None, az=None
+            date=None, pfizer=None, moderna=None, az=None, janssen=None
         )
 
         if R == None:
@@ -209,6 +209,7 @@ def _parse_vaccines_supplied_by_manufacturer_cum(
         moderna = None
         pfizer = None
         az = None
+        janssen = None
 
         if len(elements) == 1:
             el = elements[0]
@@ -235,6 +236,7 @@ def _parse_vaccines_supplied_by_manufacturer_cum(
                 pfizer=pfizer,
                 moderna=moderna,
                 az=az,
+                janssen=janssen,
             )
         )
 
