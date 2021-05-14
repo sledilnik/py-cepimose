ca11843747ff4cedc14749f03e3e7525fd328ff8
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-05-14 11:18:54 +0200

    test(by-day): assert two consecutive rows
    two consecutive rows with same data except date

--- test/test.py
+++ test/test.py
@@ -30,6 +30,8 @@ class CepimoseTestCase(unittest.TestCase):
         #! NIJZ is changing data tests could fail in the future
         assertRow(data[9], datetime.datetime(2021, 1, 5), 15711, 0)
         assertRow(data[22], datetime.datetime(2021, 1, 18), 48745, 315)
+        assertRow(data[41], datetime.datetime(2021, 2, 6), 56066, 44924)
+        assertRow(data[42], datetime.datetime(2021, 2, 7), 56066, 44924)
 
         self.assertDatesIncreaseSince(data, datetime.datetime(2020, 12, 27))
 
