f3db1cf6709ed42c677033ce991c42203973a960
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-06-11 16:22:24 +0200

    fix(vaccinations_by_manufacturer_used): zeros...
    ...instead of None
    Add missing test

--- test/test.py
+++ test/test.py
@@ -553,6 +553,55 @@ class CepimoseTestCase(unittest.TestCase):
     def test_vaccinations_by_manufacturer_used(self):
         data = cepimose.vaccinations_by_manufacturer_used()
 
+        self.assertDatesIncreaseSince(data, datetime.datetime(2020, 12, 26))
+
+        def assertRow(row, expected_date, expected):
+            print(row)
+            expected_pfizer, expected_moderna, expected_az, expected_janssen = expected
+            self.assertEqual(row.date, expected_date)
+            if expected_pfizer != None:
+                self.assertAlmostEqual(row.pfizer, expected_pfizer, delta=50)
+            else:
+                self.assertEqual(row.pfizer, expected_pfizer)
+            if expected_moderna != None:
+                self.assertAlmostEqual(row.moderna, expected_moderna, delta=50)
+            else:
+                self.assertEqual(row.moderna, expected_moderna)
+            if expected_az != None:
+                self.assertAlmostEqual(row.az, expected_az, delta=50)
+            else:
+                self.assertEqual(row.az, expected_az)
+            if expected_janssen != None:
+                self.assertAlmostEqual(row.janssen, expected_janssen, delta=50)
+            else:
+                self.assertEqual(row.janssen, expected_janssen)
+
+        assertRow(data[20], datetime.datetime(2021, 1, 16), [323, None, None, None])
+        assertRow(data[23], datetime.datetime(2021, 1, 19), [2103, 66, None, None])
+        assertRow(data[38], datetime.datetime(2021, 2, 3), [4805, None, 1, None])
+        assertRow(data[42], datetime.datetime(2021, 2, 7), [None, None, None, None])
+        assertRow(data[50], datetime.datetime(2021, 2, 15), [28, 40, 18, None])
+        assertRow(data[79], datetime.datetime(2021, 3, 16), [606, 445, None, None])
+        assertRow(data[98], datetime.datetime(2021, 4, 4), [None, 1594, None, None])
+        assertRow(data[99], datetime.datetime(2021, 4, 5), [1, None, None, None])
+        assertRow(data[120], datetime.datetime(2021, 4, 26), [None, None, 385, None])
+        assertRow(data[134], datetime.datetime(2021, 5, 10), [46, 141, 2080, 717])
+
+        for row in data:
+            print(row)
+            self.assertTrue(
+                row.date >= datetime.datetime(2021, 1, 8, 0, 0) or row.moderna == None,
+                f"Too early for Moderna usage: {row}",
+            )
+            self.assertTrue(
+                row.date >= datetime.datetime(2021, 1, 28, 0, 0) or row.az == None,
+                f"Too early for Astra Zeneca usage: {row}",
+            )
+            self.assertTrue(
+                row.date >= datetime.datetime(2021, 4, 14, 0, 0) or row.janssen == None,
+                f"Too early for Janssen usage: {row}",
+            )
+
     # @unittest.skip("TODO")
     def test_vaccinations_gender_by_date(self):
         test_date1 = datetime.datetime(2021, 1, 10)
